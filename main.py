f1e8ab48062da7b85fd40d2cc6205cb739c82556
Daniil.Krant <daniil.krant@globallogic.com>
2021-07-21 12:48:11 +0300

    Replace scheduler with async
    Signed-off-by: Daniil.Krant <daniil.krant@globallogic.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sched
+import asyncio
+
 import time
 import datetime
 
@@ -45,7 +46,7 @@ def get_next_image():
     with mss() as sct:
         monitor = sct.monitors[2]
         screenshot = np.array(sct.grab(monitor))
-        screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2RGBA)
+        screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
         return screenshot
     # return cv2.imread(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_files/screenshot_eng.jpg'))
 
@@ -68,7 +69,6 @@ def show(image_to_process, title):
 
 
 def prepare_for_ocr(active_user_image):
-    active_user_image = cv2.cvtColor(active_user_image, cv2.COLOR_BGR2GRAY)
     active_user_image = cv2.threshold(active_user_image, 150, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     x, y, w, h = cv2.boundingRect(active_user_image)
     cropped = active_user_image[round(h * 0.8):h, round(w * 0.01):round(w * 0.5)]
@@ -100,23 +100,24 @@ def process(processed_image):
     add_speaker_to_stat(speaker_name)
 
 
-def loop(scheduler):
+def loop():
     captured_image = get_next_image()
     process(captured_image)
     get_stats()
-    scheduler.enter(1, 1, loop, (scheduler,))
-
-
-def start_loop():
-    s = sched.scheduler(time.time, time.sleep)
-
-    s.enter(1, 1, loop, (s,))
-    s.run()
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
+        exit()
+
+
+async def start_loop():
+    timer = asyncio.get_event_loop().time
+    while True:
+        await asyncio.sleep(1 - timer() % 1)
+        loop()
 
 
 if __name__ == '__main__':
-    start_loop()
+    asyncio.run(start_loop())
+
 
 # $ sudo apt install tesseract-ocr
